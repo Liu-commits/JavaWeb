6c1eafd8897cb9ce3bef14e6b8caebcc6739251a
Liu-commits <1656389800@qq.com>
2020-02-05 13:05:37 +0800

    JavaBean转换XML

---
+++ XStream(JavaBeanToXML)/src/com/lyq/Demo/City.java
@@ -0,0 +1,34 @@
+package com.lyq.Demo;
+
+public class City {
+	private String name;//ÊÐÃû
+	private String desc;//ÃèÊö
+	
+	
+	
+	
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getDesc() {
+		return desc;
+	}
+	public void setDesc(String desc) {
+		this.desc = desc;
+	}
+	
+	public City(String name, String desc) {
+		super();
+		this.name = name;
+		this.desc = desc;
+	}
+	@Override
+	public String toString() {
+		return "City [name=" + name + ", desc=" + desc + "]";
+	}
+	
+	
+}
